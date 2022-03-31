2750656532e54ebbcf67229417ab52de54446257
Danperad <kokorin0521@gmail.com>
2022-03-31 19:32:35 +0300

    add webpack and gulp

---
+++ main/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'none',
+    entry: {
+        app: path.join(__dirname, 'src', 'index.tsx')
+    },
+    target: 'web',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: '/node_modules/'
+            }
+        ],
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'public', 'index.html')
+        })
+    ]
+}
